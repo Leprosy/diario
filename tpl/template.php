2c1f5039eaecc6e09f63a850ae0ff19aa27f8003
Miguel Rojas <miguel@nimbic.com>
2014-02-04 16:06:41 -0300

    Template modified to work out number or words in non featured articles.

--- tpl/template.php
+++ tpl/template.php
@@ -49,14 +49,15 @@
             <?php if (count($stream)) : ?>
             <section id="normal">
                 <?php foreach($stream as $post) : ?>
-                <article class="sub<?php if ($post->social >= Html::FEAT) : ?> feat<?php endif; ?>">
+                <?php $feat = $post->social >= Html::FEAT ?>
+                <article class="sub<?php if ($feat) : ?> feat<?php endif; ?>">
                     <h1><a href="<?php echo $post->link ?>" target="_blank"><?php echo $post->title ?></a></h1>
                     <p class="meta">Publicado <?php echo Html::relativeDate($post->date) ?> vía <?php echo $post->source ?></p>
                     <p>
-                        <?php if ($post->social >= Html::FEAT && $post->thumb) : ?>
+                        <?php if ($feat && $post->thumb) : ?>
                             <img src="<?php echo $post->thumb ?>" />
                         <?php endif; ?>
-                        <?php echo $post->content ?> ... <a class="more" href="<?php echo $post->link ?>" target="_blank">[Leer más]</a>
+                        <?php echo $feat ? $post->content : Html::words($post->content, 35) ?> ... <a class="more" href="<?php echo $post->link ?>" target="_blank">[Leer más]</a>
                     </p>
                 </article>
                 <?php endforeach; ?>
