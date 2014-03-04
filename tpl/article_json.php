c608247079ab19aaa7bca15add7334554de2f314
Miguel Rojas <miguel@nimbic.com>
2014-03-04 11:42:37 -0300

    Better JSON encoding and code refactor.

--- tpl/article_json.php
+++ tpl/article_json.php
@@ -1,11 +1,3 @@
-<?php $feat = $post->social >= Config::socialFeat ?>
-{
-    "url": "<?php echo $post->link ?>",
-    "title": "<?php echo $post->title ?>",
-    "reldate": "<?php echo Html::relativeDate($post->date) ?>",
-    "date": "<?php echo $post->date ?>",
-    "source": "<?php echo $post->source ?>",
-    "thumb": "<?php if ($post->thumb) : ?><?php echo $post->thumb ?><?php endif; ?>",
-    "content": "<?php echo $post->content ?>",
-    "feat": <?php echo $feat ? "true" : "false" ?>
-}
+<?php $post->feat = $post->social >= Config::socialFeat ?>
+<?php $post->reldate = Html::relativeDate($post->date) ?>
+<?php echo json_encode($post, JSON_HEX_QUOT) ?>
--- tpl/template_json.php
+++ tpl/template_json.php
@@ -1,26 +1,9 @@
 {
     "feat": [
     <?php if (count($feat)) : ?>
-    {
-        "url": "<?php echo $feat[0]->link ?>",
-        "title": "<?php echo $feat[0]->title ?>",
-        "reldate": "<?php echo Html::relativeDate($feat[0]->date) ?>",
-        "date": "<?php echo $feat[0]->date ?>",
-        "source": "<?php echo $feat[0]->source ?>",
-        "thumb": "<?php if ($feat[0]->thumb) : ?><?php echo $feat[0]->thumb ?><?php endif; ?>",
-        "content": "<?php echo $feat[0]->content ?>"
-    }
-    <?php for ($i = 1; $i < count($feat); ++$i) : ?>
-    ,{
-        "url": "<?php echo $feat[$i]->link ?>",
-        "title": "<?php echo $feat[$i]->title ?>",
-        "reldate": "<?php echo Html::relativeDate($feat[$i]->date) ?>",
-        "date": "<?php echo $feat[$i]->date ?>",
-        "source": "<?php echo $feat[$i]->source ?>",
-        "thumb": "<?php if ($feat[$i]->thumb) : ?><?php echo $feat[$i]->thumb ?><?php endif; ?>",
-        "content": "<?php echo $feat[$i]->content ?>"
-    }
-    <?php endfor; ?>
+    <?php foreach ($feat as $i => $post) : ?>
+    <?php include('tpl/article_json.php') ?><?php if ($i < 3) : ?>,<?php endif; ?>
+    <?php endforeach; ?>
     <?php endif; ?>
     ],
 
